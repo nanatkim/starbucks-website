5a9de1467d2ce0cd1de5f284ae47b4286956fa6d
Natali <nanatkim11@gmail.com>
2021-03-05 17:28:53 -0300

    [style] - make website responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,33 @@ import { render } from "react-dom";
 import Link from "next/link";
 
 import styles from "../styles/components/Header.module.css";
+import { useState } from "react";
 
 export function Header() {
+  const [menu, setMenu] = useState(false);
+  function toggleMenu() {
+    setMenu(!menu);
+  }
+
   return (
     <header className={styles.container}>
       <a href="#">
         <img src="logo.png" alt="Starbucks" className={styles.logo} />
       </a>
 
-      <ul className={styles.options}>
+      {menu ? (
+        <div
+          className={`${styles.toggle} ${styles.close}`}
+          onClick={toggleMenu}
+        ></div>
+      ) : (
+        <div
+          className={`${styles.toggle} ${styles.open}`}
+          onClick={toggleMenu}
+        ></div>
+      )}
+
+      <ul className={`${styles.options} ${menu ? styles.active : ""}`}>
         <li>
           <a href="#">Home</a>
         </li>
